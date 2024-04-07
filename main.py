526d33436dd99e37599d84afe2033486858efb12
Momen2410 <momenwalied2002@gmail.com>
2024-04-07 15:23:02 +0200

    Data validation done

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.statge_01_data_ingestion import DataIngestionTrainigPipleine
+from src.textSummarizer.pipeline.statge_02_data_validation import DataValidationTrainigPipleine
 
 
 statge_name = 'Data Ingestion'
-
-
 try: 
     logger.info(f'>>>>>>>>>> statge {statge_name} started <<<<<<<<<<<<<')
     data_ingestion = DataIngestionTrainigPipleine()
     data_ingestion.main()
+    logger.info(f'>>>>>>>>>> statge {statge_name} compleated <<<<<<<<<<<<<  \n\nx=============x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+statge_name = 'Data Validation'
+try: 
+    logger.info(f'>>>>>>>>>> statge {statge_name} started <<<<<<<<<<<<<')
+    data_validation = DataValidationTrainigPipleine()
+    data_validation.main()
     logger.info(f'>>>>>>>>>> statge {statge_name} compleated <<<<<<<<<<<<<  \n\nx=============x')
 except Exception as e:
     logger.exception(e)
