25ef1c17550118dda9f92769036fb749d1bd6e02
Momen2410 <momenwalied2002@gmail.com>
2024-04-08 16:09:32 +0200

    Model Training and evaluation is Done

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.statge_01_data_ingestion import DataIngestionTrainigPipleine
 from src.textSummarizer.pipeline.statge_02_data_validation import DataValidationTrainigPipleine
 from src.textSummarizer.pipeline.statge_03_data_transformation import DataTransformationTrainingPipeline
+from src.textSummarizer.pipeline.statge_04_model_trainer import ModelTrainingPipeline
+from src.textSummarizer.pipeline.statge_05_model_evaluation import ModelEvaluationPipeline
+
 
 statge_name = 'Data Ingestion'
 try: 
@@ -30,6 +33,27 @@ try:
     logger.info(f'>>>>>>>>>> statge {statge_name} started <<<<<<<<<<<<<')
     data_tranformation = DataTransformationTrainingPipeline()
     data_tranformation.main()
+    logger.info(f'>>>>>>>>>> statge {statge_name} compleated <<<<<<<<<<<<<  \n\nx=============x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+statge_name = 'Model Training'
+try: 
+    logger.info(f'>>>>>>>>>> statge {statge_name} started <<<<<<<<<<<<<')
+    model_train = ModelTrainingPipeline()
+    model_train.main()
+    logger.info(f'>>>>>>>>>> statge {statge_name} compleated <<<<<<<<<<<<<  \n\nx=============x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+statge_name = 'Model Evaluation'
+try: 
+    logger.info(f'>>>>>>>>>> statge {statge_name} started <<<<<<<<<<<<<')
+    model_train = ModelEvaluationPipeline()
+    model_train.main()
     logger.info(f'>>>>>>>>>> statge {statge_name} compleated <<<<<<<<<<<<<  \n\nx=============x')
 except Exception as e:
     logger.exception(e)
